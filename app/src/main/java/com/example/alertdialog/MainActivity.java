eb6f8c53b1e890d0df839147710e0c7b21c3e7ba
nurullahkarakoca <nurullahkarakoca@gmail.com>
2022-01-28 13:22:48 +0300

    initial commit

---
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.alertdialog;
+
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void save(View view){
+        AlertDialog.Builder alert=new AlertDialog.Builder(this);
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Did not save", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.show();
+    }
+}
